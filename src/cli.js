a9c6dc62d2b583258848f0453b01538303cf99aa
Zarema <zarema.khalilova@gmail.com>
2018-05-05 17:47:06 +0300

    Add styling for the console report

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,24 @@
 const everylint = require('./')
+const chalk = require('chalk')
+
+const STYLES = {
+  error: chalk.red,
+  warning: chalk.yellow,
+  eslint: chalk.hex('#00f'),
+  stylelint: chalk.black,
+}
+const stylish = (text) => typeof STYLES[text] !== 'undefined' ? STYLES[text](text.padStart(10)) : text.padStart(10)
 
 const showReport = (files) => {
   files.forEach(file => {
-    console.log(file.name)
+    console.log(chalk.bold.underline(file.name))
 
     file.messages.forEach(message => {
       const {linter, type, line, column, text} = message
 
-      console.log(`  ${linter} ${type} - ${line}:${column} ${text}`)
+      process.stdout.write(`  ${stylish(linter)} ${stylish(type)} `)
+      process.stdout.write(chalk.gray(`${line.toString().padStart(4)}:${column.toString().padEnd(4)} `))
+      process.stdout.write(`${text}\n`)
     })
   })
 }
